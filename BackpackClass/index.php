06ecb2258994c32a2ef0c21cec8e3c36a6c685e8
RyanKimmitt <78812561+RyanKimmitt@users.noreply.github.com>
2021-12-14 12:12:36 -0700

    Add files via upload

---
+++ BackpackClass/index.php
@@ -0,0 +1,74 @@
+<?php
+
+
+class Backpack
+{
+    // Properties
+    public $colour;
+    public $size;
+    public $items = [];
+    public $open = false;
+
+    // methods
+    function set_colour($colour)
+    {
+
+        $this->colour = $colour;
+    }
+    function set_size($size)
+    {
+        $this->size = $size;
+    }
+    function openBag()
+    {
+        $this->open = true;
+        print_r("bag has been opened!");
+    }
+    function closeBag()
+    {
+        $this->open = false;
+        print_r("bag has been closed!");
+    }
+    function putin($item)
+    {
+        if ($this->open == false) {
+            print_r("Sir you can't put anything into a closed bag");
+        } else {
+            array_push($this->items, $item);
+            print_r($item . " has been added to your bag");
+        }
+    }
+    function takeout($item)
+    {
+        if ($this->open == false) {
+            print_r("Sir you can't take anything out of a closed bag");
+        } else {
+            if (in_array($item, $this->items)) {
+                $bag = $this->items;
+                unset($bag($item));
+                print_r($item . " has been removed from your bag");
+            }
+        }
+    }
+}
+
+
+$bagOne = new Backpack;
+$bagOne->set_colour("blue");
+$bagOne->set_size("small");
+
+$bagTwo = new Backpack;
+$bagTwo->set_colour("red");
+$bagTwo->set_size("medium");
+
+$bagThree = new Backpack;
+$bagThree->set_colour("green");
+$bagThree->set_size("large");
+
+$bagTwo->openBag();
+$bagTwo->putin("Lunch");
+$bagTwo->putin("Jacket");
+$bagTwo->closeBag();
+$bagTwo->openBag();
+$bagTwo->takeout("Jacket");
+$bagTwo->closeBag();
